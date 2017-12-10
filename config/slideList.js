7d658abd70d1d7859b200a90db60c139cc40183c
jiejiss <c141028@protonmail.com>
2017-12-10 10:32:12 +0800

    update image slide show

--- config/slideList.js
+++ config/slideList.js
@@ -1,6 +1,13 @@
 module.exports = {
     "midishow-mididownloader-vbs": {
-        images: ["slider-img-1.jpg", "slider-img-2.jpg"],
-        "parent-query": "div#slideDiv"
+        images: [
+            "slider-img-1.jpg",
+            "slider-img-2.jpg",
+            "slider-img-3.jpg",
+            "slider-img-4.jpg"
+        ],
+        insertAfter: "h1",
+        nodeIndex: 2,
+        width: 600
     }
 };
